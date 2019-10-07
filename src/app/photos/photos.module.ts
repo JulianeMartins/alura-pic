bf1ed2fd0f138d452207b852d0a2b797b117db79
Juliane Martins de Vargas <martinsjuliane@icloud.com>
2019-10-07 17:11:36 -0300

    single page applications e rotas

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-@NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ],
-    imports: [ HttpClientModule ]
-})
-export class PhotosModule {
+import { PhotoComponent } from "./photo/photo.component";
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { CommonModule } from '@angular/common';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
-}
+@NgModule({
+    declarations: [
+      PhotoComponent,
+      PhotoListComponent,
+      PhotoFormComponent
+    ],
+    imports: [
+      HttpClientModule,
+      CommonModule
+    ]
+})
+export class PhotosModule { }
