8b0283edc658bfbbd9423e539ada343d7fa99599
Juliane Martins de Vargas <martinsjuliane@icloud.com>
2019-10-07 11:35:21 -0300

    cria lista de fotos com um componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'alura-pic';
+
+  photos = [
+    {
+      url: 'https://abrilveja.files.wordpress.com/2016/05/alx_vista_de_mercurio_original5.jpeg',
+      description: 'Mercúrio'
+    },
+    {
+      url:'https://www.infoescola.com/wp-content/uploads/2008/04/planeta-v%C3%AAnus_60584053.jpg',
+      description:'Vênus'
+    }
+  ];
 }
