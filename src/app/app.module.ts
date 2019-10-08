5a266a91cae7a7808ed76be7993ea530653c114e
Juliane Martins de Vargas <martinsjuliane@icloud.com>
2019-10-08 15:20:17 -0300

    implementação do componente de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import { PhotosModule } from './photos/photos.module';
 import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ErrorsModule } from './errors/errors.module';
     AppRoutingModule,
     PhotosModule,
     AppRoutingModule,
-    ErrorsModule
+    ErrorsModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
