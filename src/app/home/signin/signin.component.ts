c5108180a40776071847fc898785065756e4ab48
Juliane Martins de Vargas <martinsjuliane@icloud.com>
2019-10-08 17:26:11 -0300

    ViewChild - obtendo referências do template

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/auth.service';
 import { Router } from '@angular/router';
@@ -9,7 +9,9 @@ import { Router } from '@angular/router';
 export class SignInComponent implements OnInit {
   
   loginForm: FormGroup;
-  
+  @ViewChild('userNameInput', {static: false}) userNameInput: ElementRef<HTMLInputElement>;
+
+
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
@@ -33,6 +35,7 @@ export class SignInComponent implements OnInit {
         err => {
           console.log(err);
           this.loginForm.reset();
+          this.userNameInput.nativeElement.focus();
         }
       );
   }
