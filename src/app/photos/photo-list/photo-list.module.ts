b49c2b1df7bf961e2fd1079a6fc236c5488bd98d
Juliane Martins de Vargas <martinsjuliane@icloud.com>
2019-10-14 14:55:39 -0300

    modificando 'directives' para lugar correto

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from 'src/app/shared/components/card/card.module';
 import { SearchComponent } from './search/search.component';
-import { DarkenOnHoverModule } from './search/directives/darken-on-hover/darken-on-hover.module';
+import { DarkenOnHoverModule } from '../../shared/directives/darken-on-hover/darken-on-hover.module';
 
 @NgModule({
   declarations: [
