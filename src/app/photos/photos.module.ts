45fbc7633b686eb50388065599724330c685da66
Juliane Martins de Vargas <martinsjuliane@icloud.com>
2019-10-14 15:33:52 -0300

    rotas parametrizadas

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { PhotoFormModule } from './photo-form/photo-form.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
 import { PhotoModule } from './photo/photo.module';
+import { PhotoDetailsModule } from './photo-details/photo-details.module';
 
 @NgModule({
     imports: [
       PhotoModule,
       PhotoFormModule,
-      PhotoListModule
+      PhotoListModule,
+      PhotoDetailsModule
     ]
 })
 export class PhotosModule { }
