15ce7ef4811f271c5172eb1f1de3da7982cd5137
Juliane Martins de Vargas <martinsjuliane@icloud.com>
2019-10-15 17:36:21 -0300

    implementando mensagem no upload da foto

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { PhotoService } from '../photo/photo.service';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
 
 @Component({
   selector: 'ap-photo-form',
@@ -18,7 +19,8 @@ export class PhotoFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private photoService:  PhotoService,
-    private router: Router
+    private router: Router,
+    private alertService: AlertService
   ) { }
 
   ngOnInit() {
@@ -34,7 +36,10 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments').value;
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => this.router.navigate(['']))
+      .subscribe(() => {
+        this.alertService.success('Upload complete');
+        this.router.navigate([''])
+      });
   }
 
   handleFile(file: File) {
