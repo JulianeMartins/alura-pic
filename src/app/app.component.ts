8303e5aab4053bbdeef183b3cd9b008c7122e1be
Juliane Martins de Vargas <martinsjuliane@icloud.com>
2019-10-16 14:05:21 -0300

    modificando o title da página através do sistema de rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { map, filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title
+  ) { }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .pipe(map(() => this.activatedRoute))
+      .pipe(map(route => {
+        while(route.firstChild) route = route.firstChild;
+        return route;
+      }))
+      .pipe(switchMap(route => route.data))
+      .subscribe(event => this.titleService.setTitle(event.title));
+  }
+}
